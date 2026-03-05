cd0d95395fb3b154ae21ef395c016f9bca3c4501
sdwivedi <sdwivedi@contentactive.com>
2026-03-05 19:10:29 +0530

    Added minor changes

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={ fontFamily: "Arial", textAlign: "center", marginTop: "100px" }>
+      <h1>Welcome to My Website</h1>
+      <p>This is a simple React page.</p>
+    </div>
+  );
+}
